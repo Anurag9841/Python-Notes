202283fd74049bf41e41a3810b9387e01c8fa6fa
Anurag Karki <69763071+Anurag9841@users.noreply.github.com>
2022-02-06 08:23:49 +0545

    Add files via upload

--- numpy.py
+++ numpy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[2]:
 
 
 import numpy as np
@@ -142,22 +142,22 @@ lspace.dtype
 
 # array using arange
 
-# In[66]:
+# In[8]:
 
 
 arr=np.arange(10)
 
 
-# In[67]:
+# In[9]:
 
 
 arr
 
 
-# In[ ]:
+# In[10]:
 
 
-
+arr.reshape(2,5) #---> USED TO RESHAPE ARRAY ARR
 
 
 # array using zeros
@@ -198,6 +198,266 @@ for i in range(0,5):
 
 
 zero1
+
+
+# In[3]:
+
+
+ide=np.identity(3) #---> used to create the identity matrix of size 3
+
+
+# In[4]:
+
+
+ide
+
+
+# In[5]:
+
+
+ide.size
+
+
+# In[6]:
+
+
+ide.shape
+
+
+# In[16]:
+
+
+ide.ravel()#---> ravel is used to create 2d array to 1d array
+
+
+# In[11]:
+
+
+Arr=np.arange(12)
+
+
+# In[12]:
+
+
+Arr
+
+
+# In[13]:
+
+
+Arr.reshape(3,4)#---> reshapes array to array with 3 rows and 4 column
+
+
+# In[14]:
+
+
+Arr.reshape(4,3)#--> reshapes array to array with 4 rows and 3 column
+
+
+# In[15]:
+
+
+Arr.ravel()#---> create 2D array into 1D array
+
+
+# Numpy some arguments and methods.
+
+# 2D array
+
+# In[17]:
+
+
+x=[[1,2,3,4],[5,6,7,8],[9,0,1,2]]
+
+
+# In[18]:
+
+
+arr=np.array(x)
+
+
+# In[19]:
+
+
+arr
+
+
+# In[20]:
+
+
+arr.sum(axis=0)#---> axis=0 is directed to  row of 2D array 
+# and sum returns the sum of diff elements of row 
+# for eg in above array sum of 1,5,9 = 15 here 1 is element of 1st row
+# 5 is element of 2nd row and 9 is element of 3rd row.
+
+
+# In[21]:
+
+
+arr.sum(axis=1)#----> axis=1 is directed to column of 2D array
+# and sum returns the sum of diff element of column
+# for eg in above array sum of 1,2,3,4 = 10 here 1 is element of 1st column
+# 2 is element of 2nd col , 3 is element of 3rd col and 4 is element of 4th col.
+
+
+# In[22]:
+
+
+arr.T #---> gives transpose of array arr
+
+
+# In[23]:
+
+
+arr
+
+
+# In[24]:
+
+
+arr.argmax()#---> returns the index of max element in arr
+
+
+# In[25]:
+
+
+arr.argmin()#---> returns the index of min element in arr
+
+
+# In[32]:
+
+
+arr.argsort(axis=1)#---> sorts each row of array and returns their index
+
+
+# In[ ]:
+
+
+arr.argsort(axis=0)#---> sorts each column and returns their index
+
+
+# In[27]:
+
+
+arr.argmax(axis=0)#---> returns the max element of each column
+
+
+# In[28]:
+
+
+arr.argmax(axis=1)#---> returns the max element of each row
+
+
+# In[29]:
+
+
+arr.argmin(axis=0)#---> returns the min element of each column
+
+
+# In[ ]:
+
+
+arr.argmin(axis=1)#---> returns the max element of each row
+
+
+# 1D array
+
+# In[33]:
+
+
+one = np.array([11,2,5,28,6,7,10])
+
+
+# In[34]:
+
+
+one.argmax()
+
+
+# In[35]:
+
+
+one.argmin()
+
+
+# In[36]:
+
+
+one.argsort()
+
+
+# Numpy mathematical operation
+
+# In[37]:
+
+
+arr1=np.identity(3)
+
+
+# In[38]:
+
+
+arr1
+
+
+# In[43]:
+
+
+x=[[1,2,3],[4,5,6],[7,8,9]]
+
+
+# In[44]:
+
+
+arr2=np.array(x)
+
+
+# In[45]:
+
+
+arr2
+
+
+# In[46]:
+
+
+arr1 + arr2
+
+
+# In[47]:
+
+
+arr1 * arr2
+
+
+# In[48]:
+
+
+arr2 -arr1
+
+
+# In[49]:
+
+
+np.sqrt(arr2)
+
+
+# In[50]:
+
+
+arr2.max()
+
+
+# In[51]:
+
+
+arr2.min()
+
+
+# In[52]:
+
+
+arr.sum()
 
 
 # In[ ]:
